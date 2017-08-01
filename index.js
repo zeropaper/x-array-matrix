4425eda8284137ad55113bc23f5e04ec440fecf0
Valentin Vago <zeropaper@irata.ch>
2017-08-01 06:51:00 +0200

    chore(step-2): renderPlainText

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ function createMatrix(rowsCount, columnsCount) {
 
   return matrix;
 }
+
+
+
+function renderPlainText(data) {
+  var rows = [];
+
+  data.forEach(function(columns) {
+    var rowString = columns.join('');
+    rows.push(rowString);
+  });
+
+  return rows.join('\n');
+
+  // // using .map()
+  // var rows = matrix.map(function(columns) {
+  //   // columns is an array ;)
+  //   return columns.join('');
+  // });
+  // // rows is also an array
+  // // (because it's what the .map() function returns)
+  // // the "\n" is the code for a new line in a string
+  // return rows.join('\n');
+}
+
+document.querySelector('#step-2').textContent = renderPlainText(createMatrix(4, 4));
